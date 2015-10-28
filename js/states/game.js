efbf30dab036ea68822d066bcd654d8530abb30f
Guillaume <guillaume.moreau@ymail.com>
2015-10-28 20:08:58 +0100

    Coin lors du click sur le canard

--- js/states/game.js
+++ js/states/game.js
@@ -10,15 +10,22 @@ var gameState = {
     },
 
     create : function(){
-      game.add.sprite(100,100,"canard_template1");
+      this.canard = game.add.sprite(100,100,"canard_template1");
       this.coin = game.add.audio("sound_coin1");
 
       this.coin.play();
+
+      this.canard.events.onInputDown.add(playCoin, this);
+
     },
 
     update : function(){
 
 
     },
+
+    playCoin: function(){
+      this.coin.play();
+    }
 
 };
